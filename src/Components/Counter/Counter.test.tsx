aad9787a8677e8e5598551ec54fd6220fe7739d7
osmanbeshirov <osman.beshirov@gmail.com>
2022-11-11 02:53:31 +0300

    unit testing 2

--- src/Components/Counter/Counter.test.tsx
+++ src/Components/Counter/Counter.test.tsx
@@ -3,6 +3,38 @@ import userEvent from '@testing-library/user-event';
 
 import Counter from './index';
 
-test('increase btn', () => {
-    render(<Counter />);
+describe('Counter tester', () => {
+
+    let number: HTMLElement, increaseBtn: HTMLElement, decreaseBtn: HTMLElement;
+
+    //beforeEach - her test kodundan evvel isleyecek olan section
+    //beforeAll - butun test kodlarindan once bir defe isleyecek olan section
+    
+    //afterAll - butun test kodlari yoxlanilib bitenden sonra bir defe isleyecek olan section
+    //afterEach - her testden sonra isleyecek olan section 
+
+    beforeEach(() => {
+        render(<Counter />);
+        number = screen.getByText('0');
+        increaseBtn = screen.getByText('Increase');
+        decreaseBtn = screen.getByText('Decrease');
+
+    })
+
+    test('increase btn', () => {
+
+        userEvent.click(increaseBtn);
+
+        expect(number).toHaveTextContent('1')
+    })
+
+
+    test('decrease btn', () => {
+
+        userEvent.click(decreaseBtn);
+
+        expect(number).toHaveTextContent('-1')
+    })
+
 })
+
